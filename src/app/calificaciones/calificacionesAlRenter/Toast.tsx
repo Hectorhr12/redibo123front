527d2cc4497c9d762545cc6580de668aaceddd06
Hectorhr12 <202206428@est.umss.edu>
2025-05-16 16:23:40 -0400

    Update Toast.tsx

--- src/app/calificaciones/calificacionesAlRenter/Toast.tsx
+++ src/app/calificaciones/calificacionesAlRenter/Toast.tsx
@@ -3,23 +3,23 @@ import { useEffect } from "react"
 
 interface ToastProps {
   message: string
-  onClose: () => void
+  onCloseAction: () => void
 }
 
-export default function Toast({ message, onClose }: ToastProps) {
+export default function Toast({ message, onCloseAction }: ToastProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
-      onClose()
-    }, 5000)
+      onCloseAction()
+    }, 3000)
 
     return () => clearTimeout(timer)
-  }, [onClose])
+  }, [onCloseAction])
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="bg-black text-white px-6 py-4 rounded-xl shadow-2xl relative animate-in fade-in zoom-in-75 duration-300 max-w-sm w-full text-center">
         <button
-          onClick={onClose}
+          onClick={onCloseAction}
           className="absolute top-2 right-3 text-white text-lg font-bold hover:text-gray-300"
         >
           ×
